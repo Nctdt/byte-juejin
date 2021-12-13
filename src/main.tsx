ec49bda85e7e85124e61f21b666ad760361eb25c
Nctdtman <137032445@qq.com>
2021-12-13 10:05:32 +0800

    update: use suspense to loading

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,22 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { RecoilRoot } from 'recoil'
-import { Posts } from './feature/Posts'
+import { Spin } from '@douyinfe/semi-ui'
 
 import './index.css'
+import { Posts } from './feature/Posts'
 
 ReactDOM.render(
   <React.StrictMode>
     <RecoilRoot>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Posts />} />
-        </Routes>
-      </BrowserRouter>
+      <Suspense fallback={<Spin size="large" />}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Posts />} />
+          </Routes>
+        </BrowserRouter>
+      </Suspense>
     </RecoilRoot>
   </React.StrictMode>,
   document.getElementById('root'),
