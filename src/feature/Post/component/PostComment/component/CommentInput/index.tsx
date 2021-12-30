c3fa5292ad75933d94f692da0a3d7003a9b28002
Nctdtman <137032445@qq.com>
2021-12-30 14:12:06 +0800

    feat: post comment submit

--- src/feature/Post/component/PostComment/component/CommentInput/index.tsx
+++ src/feature/Post/component/PostComment/component/CommentInput/index.tsx
@@ -1,24 +1,71 @@
-import { FC } from 'react'
+import { FC, useRef, useState } from 'react'
 import { useBoolean } from 'ahooks'
+import { Button, Toast } from '@douyinfe/semi-ui'
+
+const useScript = () => {
+  const [focused, { setTrue, setFalse }] = useBoolean(false)
+  const [value, setValue] = useState('')
+  const divRef = useRef<HTMLDivElement>(null)
+
+  const handleFocus = () => {
+    setTrue()
+  }
+  const handleBlur = () => {
+    setFalse()
+  }
+  const handleInput = () => {
+    setValue(divRef.current?.innerText ?? '')
+  }
+  const handleSubmit = () => {
+    Toast.info(`提交的评论内容为: ${value}`)
+  }
+  return {
+    divRef,
+    isEmpty: value === '',
+    actived: focused || !(value === ''),
+    value,
+    handleFocus,
+    handleBlur,
+    handleInput,
+    handleSubmit,
+  }
+}
 
 export const CommentInput: FC = () => {
-  const [focused, { setTrue, setFalse }] = useBoolean(false)
+  const {
+    divRef,
+    actived,
+    isEmpty,
+    handleFocus,
+    handleBlur,
+    handleInput,
+    handleSubmit,
+  } = useScript()
   return (
-    <div
-      contentEditable
-      spellCheck="false"
-      placeholder="输入评论"
-      className={`transition-all border rounded-md p-2 w-full h-16 text-sm text-gray-400 outline-none ${
-        focused
-          ? 'border-blue-400 bg-white'
-          : 'border-gray-100 bg-gray-100 before:content-[attr(placeholder)]'
-      }`}
-      onFocus={() => {
-        setTrue()
-      }}
-      onBlur={() => {
-        setFalse()
-      }}
-    />
+    <>
+      <div
+        contentEditable
+        spellCheck="false"
+        placeholder="输入评论"
+        ref={divRef}
+        className={`transition-all duration-150 border rounded-md p-2 w-full h-16 text-sm text-gray-400 outline-none ${
+          actived ? 'border-blue-400 bg-white' : 'border-gray-100 bg-gray-100'
+        } ${isEmpty ? 'before:content-[attr(placeholder)]' : ''}`}
+        onFocus={handleFocus}
+        onBlur={handleBlur}
+        onInput={handleInput}
+      />
+      {actived && (
+        <div className="flex justify-end mt-2">
+          <Button
+            theme="solid"
+            className="p-5 rounded-md font-normal"
+            disabled={isEmpty}
+            onClick={handleSubmit}>
+            发表评论
+          </Button>
+        </div>
+      )}
+    </>
   )
 }
